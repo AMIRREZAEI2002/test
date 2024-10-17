5c54e09b61cb22cc5a88c78cf7ea717cbad469f2
AMIRREZAEI2002 <amirrezaeiofficial2002@gmail.com>
2024-10-17 17:08:59 +0330

    first commit

---
+++ file1/js1.js
@@ -0,0 +1,9 @@
+import name1 from "../file3/jsmain.js"
+
+let nu1 = document.getElementById('nu1')
+let nu2 = document.getElementById('nu2')
+let result = document.getElementById('result')
+
+
+
+nu1.addEventListener('click',name1)
